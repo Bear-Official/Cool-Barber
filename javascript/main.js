deac5492976b59d051227ad1011ff2a2cb515e01
Bear <danekgdgdg@gmail.com>
2023-04-04 15:31:56 +0300

    [Added] "Gallery"

--- javascript/main.js
+++ javascript/main.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
             },
         ],
     });
-}); 
+});
 
 const menuToggle = document.querySelector("#menu-toggle");
 const body = document.querySelector("body");
@@ -26,4 +26,8 @@ menuToggle.addEventListener("click", function () {
     body.classList.toggle("overflow-hidden");
 });
 
+lightGallery(document.getElementById("lightgallery")); 
+lightGallery(document.getElementById("lightgallery-2")); 
+lightGallery(document.getElementById("lightgallery-3")); 
+lightGallery(document.getElementById("lightgallery-4")); 
 
